151d6e491331149688d5b113110c7e10ebf5cbfa
Your Name <melissasad201@gmail.com>
2026-08-14 23:37:30 +0000

    push

--- exploring_your_first_network/test_loopback.sh
+++ exploring_your_first_network/test_loopback.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ping -c 4 "$(ip -4 addr show scope host | tr -s ' ' | cut -d ' ' -f 4 | cut -d '/' -f 1 | head -n 1)"
+ping -c 4 "$(ip -4 addr show scope host | grep 'inet ' | head -n 1 | tr -s ' ' | cut -d ' ' -f 3 | cut -d '/' -f 1)"
